f120e052cc6f01791ded2fd43fb23857c760aa6c
Anna Pang <anna.pang@uzh.ch>
2024-03-07 17:27:22 +0100

    helper changed

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -7,7 +7,7 @@ import { isProduction } from "./isProduction"
  * @returns {string}
  */
 export const getDomain = () => {
-  const prodUrl = "https://sopra-fs24-pang-anna-client.oa.r.appspot.com/"
+  const prodUrl = "https://sopra-fs24-pang-anna-server.oa.r.appspot.com/"
   const devUrl = "http://localhost:8080"
 
   return isProduction() ? prodUrl : devUrl
